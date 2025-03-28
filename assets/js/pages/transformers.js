63a2421bfb7357f9edeeb5e42cbc5a15516799b9
rocordov <rocordov@gmail.com>
2025-03-28 09:03:51 -0700

    Auto-commit: changes detected on 2025-03-28 09:03:51

--- assets/js/pages/transformers.js
+++ assets/js/pages/transformers.js
@@ -9,7 +9,9 @@
  */
 
 //import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.17.2/+esm';
-import { pipeline } from 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.0.0';
+//import { pipeline } from 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.0.0';
+import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@3.0.0';
+
 // Configuration object
 const CONFIG = {
     model: 'Xenova/SmolLM2-135M-Instruct',
