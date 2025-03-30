97d013310eaa52b89a585628c34e913be5f866e6
rocordov <rocordov@gmail.com>
2025-03-29 17:52:01 -0700

    Auto-commit: changes detected on 2025-03-29 17:52:01

--- assets/js/components/component-loader.js
+++ assets/js/components/component-loader.js
@@ -126,7 +126,9 @@ function loadCustomizationsScript() {
 document.addEventListener('DOMContentLoaded', () => {
   // Load client.js and execute functions
   const script = document.createElement('script');
+
   script.src = `${rootPath}/assets/js/components/client.js`;
+  
   script.onload = () => {
     resetSessionStorage();
     dataCollection();
